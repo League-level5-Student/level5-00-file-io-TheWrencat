0d54fe3f4805655c9b4a4caa9eaa6e6cf9e73cab
Your Name <you@example.com>
2019-04-04 19:58:44 -0700

    started

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,22 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
-	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+
+		// Create a program that takes a message from the user and saves it to a file.
+		try {
+
+			FileWriter fw1 = new FileWriter("src/_00_Intro_To_File_Input_and_Output/test2.txt");
+			fw1.write(JOptionPane.showInputDialog("Write to a file."));
+
+			fw1.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,7 +1,18 @@
 package _02_File_Encrypt_Decrypt;
 
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	// Create a program that takes a message from the user.
 	// Use the methods in the String and Character classes to save
 	// an encrypted form of the message to a file
+
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("Write a message to encrypt.");
+		for (int i = 0; i < input.length(); i++) {
+			String t = input.substring(i, i + 1);
+			input += input.substring(input.length() - i, input.length() - i - 1);
+		}
+	}
+
 }
